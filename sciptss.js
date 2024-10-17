52b7f5cf49eaf05a5d33b188f00c9b3effc9b41e
Andony Par <andonypar@Andonys-Mac-Studio.local>
2024-10-17 14:54:30 -0600

    Cambios 4, 5 ,6

--- sciptss.js
+++ sciptss.js
@@ -6,4 +6,13 @@
             const activeContent = document.getElementById(moduleId);
             activeContent.classList.add('active');
         }
+
+
+        function showContent(moduleId) {
+            const contents = document.querySelectorAll('.module-content-andony1');
+            contents.forEach(content => content.classList.remove('active'));
+
+            const activeContent = document.getElementById(moduleId);
+            activeContent.classList.add('active');
+        }
   
